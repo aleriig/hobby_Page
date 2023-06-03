301f146984fc74a123fcf76310a2de0dabb967f6
Alejo Rios <alejoriosiglesias@gmail.com>
2023-06-03 16:58:17 +0100

    feat: change date's font size and color

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
+
+// displaying current date
 const date = new Date().toLocaleDateString("en-GB");
     document.getElementById("date").innerText = date;
+// changing size
+    document.getElementById("date").style.fontSize = '2.5rem';
+// changing color
+    document.getElementById("date").style.color = '#52A145';
+    
+
